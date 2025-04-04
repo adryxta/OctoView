59c54845a4328d33147bb2e0aa24aef5611e376c
Adrita Choudhury <adritachoudhury2015@gmail.com>
2025-04-04 17:08:48 +0900

    Add todos and singleton

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Add the following to your `build.gradle` file:
 api.baseUrl=https://api.github.com
 api.authToken=<YourGH Token or PAT>
 ```
+
+### TODOs
+
+- [ ] Add error handling to the Retrofit service
+- [ ] Add a caching strategy to the Retrofit service
+- [ ] Add Custom Error types
+- [ ] Test for ViewModels
--- data/src/main/kotlin/dev/adryxta/octoview/data/DataModule.kt
+++ data/src/main/kotlin/dev/adryxta/octoview/data/DataModule.kt
@@ -1,35 +1,36 @@
 package dev.adryxta.octoview.data
 
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.components.SingletonComponent
 import dev.adryxta.octoview.data.api.GitHubUserApi
 import javax.inject.Named
 
+/**
+ * Module that provides dependencies for the data layer.
+ * Need dependent [String] values to create the Rest Client.
+ * @see DataModule.API_BASE_URL
+ * @see DataModule.API_AUTH_TOKEN
+ */
 @Module
 @InstallIn(SingletonComponent::class)
-class SingletonModule {
+class DataModule {
 
     @Provides
     internal fun provideApiService(
-        @Named(DataModule.API_BASE_URL) baseUrl: String,
-        @Named(DataModule.API_AUTH_TOKEN) authToken: String
+        @Named(API_BASE_URL) baseUrl: String,
+        @Named(API_AUTH_TOKEN) authToken: String
     ): GitHubUserApi {
         return GitHubUserApi.create(baseUrl, authToken)
     }
-}
 
-@Module
-@InstallIn(ViewModelComponent::class)
-abstract class DataModule {
-
-    @Binds
-    internal abstract fun bindUserRepository(
-        userRepositoryImpl: DefaultUserRepository
-    ): UserRepository
+    @Provides
+    internal fun provideUserRepository(
+        gitHubUserApi: GitHubUserApi
+    ): UserRepository {
+        return DefaultUserRepository(gitHubUserApi)
+    }
 
     companion object {
         const val API_BASE_URL = "api-base-url"
